974ae9102a414b14450b0cbc0c6368b9145ae13c
John Duncan <jduncan@perfectpowerllc.com>
2024-03-19 16:36:18 -0400

    Wether reply works now and has a 5% chance of replying with something silly

--- commands/funny/weather.js
+++ commands/funny/weather.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder } = require("discord.js");
-const { request } = require('undici');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,21 +7,28 @@ module.exports = {
       "I tell you the weather, bitches, based on the zip you give me son!"
     )
     .addStringOption(option =>
-			option
-				.setName('zipcode')
-				.setDescription('it\'s in the name dumbass!')),
+      option
+        .setName('zipcode')
+        .setDescription('it\'s in the name dumbass!')
+        .setRequired(true)
+    ),
   async execute(interaction) {
-    const query = interaction.options.getString('zipcode') ?? '10010';
-    request(
-      `http://api.weatherapi.com/v1/current.json?key=3f832c00dacc4f8c98f190014240603&q=${query}`
-    )
-      .then((dictResult) => {
-        dictResult.body.json();
-      })
-      .then((result) => {
-        console.log("result", result);
-      })
-      .catch((error) => console.log("error", error));
-    await interaction.reply("Pong!");
+    let chance = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+
+    if (chance !== 1) {
+      const query = interaction.options.getString('zipcode');
+      var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+
+      fetch(`http://api.weatherapi.com/v1/current.json?key=3f832c00dacc4f8c98f190014240603&q=${query}`, requestOptions)
+        .then(response => response.json())
+        .then(result => interaction.reply(`The current temperator in ${result.location.name} is ${result.current.temp_f}⁰F`))
+        .catch(error => console.log('error', error));
+    } else {
+      await interaction.reply("Pong!");
+    }
+
   },
 };
